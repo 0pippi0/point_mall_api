3ff8de6255e1e71ebd665487b74749174a49a34b
Lisa <j.romany91@gmail.com>
2019-08-01 16:38:46 +0900

    1st_commit

---
+++ point_mall/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from point_mall import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    path('users/', include('user.urls.user_urls')),
+    path('items/', include('item.urls.Item_urls')),
+    path('categories/', include('item.urls.category_urls')),
+    path('me/', include('user.urls.me_urls')),
+    path('media/uploads/item_images/<str:file_name>', views.image_view),
+]
